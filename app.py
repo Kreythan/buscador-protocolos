47fa3007cca395eb1ad34177a9f561b721d4989e
Kreythan <108915085+Kreythan@users.noreply.github.com>
2026-01-03 00:01:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -216,15 +216,16 @@ st.markdown("""
 components.html("""
     <div style="pointer-events: auto !important;">
         <script type="text/javascript">
+       
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                (function(){
+                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                 s1.async=true;
-                s1.src='https://embed.tawk.to/695732610a00df198198e359/1jdu9pk10';
+                s1.src='https://embed.tawk.to/695732610a00df198198e359/1jduasih0';
                 s1.charset='UTF-8';
                 s1.setAttribute('crossorigin','*');
                 s0.parentNode.insertBefore(s1,s0);
-            })();
+                })();
         </script>
     </div>
 """, height=520)
